d14125a4a3b08971dde551faeb3851b559cb51bf
AW <codingdayandnight@gmail.com>
2024-02-25 18:15:14 +0100

    connect calendar

--- src/components/connect-calendar-page.tsx
+++ src/components/connect-calendar-page.tsx
@@ -1,10 +1,16 @@
+import axios from 'axios'
 import React, { useState } from 'react'
 
 const ConnectCalendarPage: React.FC = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
 
-  const handleConnectCalendar = () => {
-    // Logic to initiate OAuth 2.0 authentication flow with Google
+  const handleConnectCalendar = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/auth-page-url')
+      window.location.href = response.data
+    } catch (error) {
+      // todo: handle error
+    }
   }
 
   const handleDisconnectCalendar = () => {
